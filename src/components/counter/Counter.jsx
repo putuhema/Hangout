4f5cc02e42957c668fd36135f016dbe768e77d05
Muchamad Choirul Anwar <mcnwr76@Muchamads-MacBook-Air.local>
2023-08-31 19:24:52 +0700

    add counter component

---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,19 @@
+import { decrement, increment } from "@/redux/counter";
+import { useDispatch, useSelector } from "react-redux";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.count);
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Counter />
+      <button onClick={() => dispatch(increment())}>+</button>
+      <input type="text" value={count} />
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  );
+};
+
+export default Counter;
--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -1,16 +1,9 @@
-import { decrement, increment } from "@/redux/counter";
-import { useDispatch, useSelector } from "react-redux";
+import Counter from "@/components/counter/Counter";
 
 const Dashboard = () => {
-  const dispatch = useDispatch();
-  const count = useSelector((state) => state.counter.count);
-  console.log(count);
   return (
     <div>
-      <h1>Dashboard</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <input type="text" value={count} />
-      <button onClick={() => dispatch(decrement())}>-</button>
+      <Counter />
     </div>
   );
 };
