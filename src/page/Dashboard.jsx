6809f8b9f1f941ab995262deaa9b4fd5849a56a7
Muchamad Choirul Anwar <mcnwr76@Muchamads-MacBook-Air.local>
2023-09-02 17:38:13 +0700

    update review

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -1,13 +1,41 @@
-import Counter from "@/components/counter/Counter";
-import StarRating from "@/components/star/StarRating";
+import EventList from "@/components/dashboard/EventList";
+import Registration from "@/components/dashboard/Registration";
+import Transaction from "@/components/dashboard/Transaction";
+import Statistic from "@/components/dashboard/Statistic";
+import { useState } from "react";
+import Container from "@/components/layout/Container";
 
 const Dashboard = () => {
+  const [path, setPath] = useState("Events");
+
+  const onChange = () => {};
+  const onClick = (e) => {
+    setPath(e.target.value);
+  };
+
+  let render;
+  if (path === "Events") {
+    render = <EventList />;
+  } else if (path === "Registration") {
+    render = <Registration />;
+  } else if (path === "Transaction") {
+    render = <Transaction />;
+  } else {
+    render = <Statistic />;
+  }
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <StarRating />
-      <Counter />
-    </div>
+    <Container className="">
+      <div className="flex w-4/5 mt-32">
+        <div className="flex flex-col items-start w-1/4 pt-24">
+          <input className="my-4 text-2xl" type="submit" value="Events" onClick={onClick} onChange={onChange} />
+          <input className="my-4 text-2xl" type="submit" value="Registration" onClick={onClick} onChange={onChange} />
+          <input className="my-4 text-2xl" type="submit" value="Transaction" onClick={onClick} onChange={onChange} />
+          <input className="my-4 text-2xl" type="submit" value="Statistic" onClick={onClick} onChange={onChange} />
+        </div>
+        <div className="ps-24 pt-8">{render}</div>
+      </div>
+    </Container>
   );
 };
 
--- src/page/Review.jsx
+++ src/page/Review.jsx
@@ -2,57 +2,105 @@ import { getOneEvent } from "@/redux/event";
 import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import StarRating from "@/components/star/StarRating";
+import Container from "@/components/layout/Container";
+import { getAllReview } from "@/redux/review";
+import { getOneUser } from "@/redux/user";
+import ReviewList from "@/components/review/ReviewList";
+import repositories from "@/repositories/repositories";
 
 const Review = () => {
   const dispatch = useDispatch();
 
   const { rating } = useSelector((state) => state.global);
   const event = useSelector((state) => state.event.events);
-  const { name } = event;
-  const review = useRef("");
+  const reviews = useSelector((state) => state.review.reviews);
+  const { user, users } = useSelector((state) => state.user);
+  const { name, location, time, date } = event;
+  const comment = useRef("");
+
+  const lastIndexUser = users.length;
+  const lastIndexReview = reviews.length;
+
+  const postAPI = async () => {
+    const dataUser = {
+      id: lastIndexUser,
+      name: user.name,
+      email: `${user.name}@mail.com`,
+    };
+    const dataReview = {
+      id: lastIndexReview,
+      rating: rating,
+      comment: comment.current.value,
+    };
+    console.log(dataUser);
+    await repositories.postAPI("users", dataUser);
+    await repositories.postAPI("reviews", dataReview);
+  };
+
+  const submit = () => {
+    postAPI();
+  };
 
   useEffect(() => {
-    dispatch(getOneEvent(1));
-  }, [dispatch]);
+    dispatch(getOneEvent(0));
+    dispatch(getAllReview());
+    dispatch(getOneUser(0));
+  }, [dispatch, reviews]);
 
   let ratingElement = rating !== 0 ? <label className="ms-2">({rating})</label> : <></>;
 
   return (
-    <div className="w-3/4 mx-auto">
-      <div className="border h-72 w-3/4 mx-auto bg-yellow-400">
-        <span>poster</span>
+    <Container>
+      <div className="w-4/5 mx-auto py-12">
+        <div className="border lg:h-72 lg:w-11/12 bg-gray-50 mx-auto p-12 rounded-lg">
+          <p className="text-6xl font-bold">{name}</p>
+          <p className="my-6">{location}</p>
+          <div className="">
+            <p className="font-semibold">Time : {time} WIB</p>
+            <p>{date}</p>
+          </div>
+        </div>
       </div>
-      <p className="text-4xl mt-6">{name}</p>
-      <hr className="my-4" />
-      <div className="flex items-end mt-20">
+      <hr className="" />
+      <div className="w-4/5 mx-auto py-6">
+        <h2 className="font-semibold text-3xl mb-8">Review</h2>
+
+        <ReviewList />
+      </div>
+      <hr />
+      <div className="w-4/5 mx-auto flex items-end mt-10">
         <div>
-          <div className="flex">
+          <div className="flex ">
             <label className="w-32 text-lg">Rating</label>
             <StarRating />
             {ratingElement}
           </div>
-          <div className="flex mt-2">
+          <div className="flex mt-2 ">
             <label className="w-32 text-lg" htmlFor="">
               Review
             </label>
             <textarea
-              className="border rounded-lg"
+              className="border rounded-lg h-72 sm:h-48 sm:w-[20rem] lg:w-[40rem] md:w-[30rem]"
               name=""
               id=""
-              cols="60"
-              rows="8"
+              // cols="60"
+              // rows="8"
               placeholder="Leave review here"
-              ref={review}
+              ref={comment}
             ></textarea>
           </div>
         </div>
-        <div className="ms-20">
-          <button className="bg-green-500 w-20 h-8 border rounded-lg hover:bg-green-400 ease-in-out duration-300">
+        <div className="ms-6 lg:ms-20">
+          <button
+            className="bg-green-500 w-20 h-8 border rounded-lg hover:bg-green-400 ease-in-out duration-300"
+            onClick={submit}
+          >
             Send
           </button>
         </div>
       </div>
-    </div>
+      <div className="h-24"></div>
+    </Container>
   );
 };
 
